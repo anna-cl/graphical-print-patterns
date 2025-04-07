bc8de930a77409776a3568bbe5538a696320030c
cla433 <anna_liang_2@sfu.ca>
2025-04-07 16:29:50 -0700

    first approach to create stacking circles like a cone

--- spiralCone.py
+++ spiralCone.py
@@ -6,21 +6,37 @@
 # https://github.com/asweigart/simple-turtle-tutorial-for-python/blob/master/draw_circles.py
 
 from turtle import *
-from tkinter import *
-from tkinter import ttk
 
 # maxRadius = int(input('Enter the max radius: '))
 # minRadius = int(input('Enter the min radius: '))
 # numCircles = int(input('Enter number of circles: '))
 
 t = turtle.Turtle()
-t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+
+# t.bgcolor("pink")
+t.penup()
+t.goto(0, -150)
+t.pendown()
 
 t.pensize(2)
+t.pencolor(169, 169, 255)
+t.speed(6)
 
 minRadius = 2
 maxRadius = 120
-input = 5
-for i in range(input + 1):
-  if i >= 1:
-		t.circle(maxRadius / i + minRadius )
+input = 20
+
+for i in range(input):
+  if i == 0:
+    t.circle(minRadius)
+  elif (i == input -1):
+    t.circle(maxRadius)
+  else:
+    t.circle(minRadius*i + (maxRadius-minRadius)/(input-2)) #this dividing does NOT create each circle in correct proportion 
+  
+  t.penup()
+  t.left(90)
+  t.forward(10)
+  t.right(90)
+  t.pendown()
+	
