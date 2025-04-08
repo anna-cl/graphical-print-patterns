1d6c13ef8f0652042994620b6d6af9cb9490974b
Anna <anna_liang_2@sfu.ca>
2025-04-08 12:16:39 -0700

    found math solution, compare two ways of expression

--- spiralCone.py
+++ spiralCone.py
@@ -1,42 +1,74 @@
-# use csil machines to run
-# https://www2.cs.sfu.ca/~wsumner/accessing-csil-remotely.html
-# https://www.sfu.ca/fas/computing/support/csil/unix/cpu-servers.html
-# python spiralCone.py
-
+# set interpreter: 3.9.7('base') conda or 3.8.2
 # https://github.com/asweigart/simple-turtle-tutorial-for-python/blob/master/draw_circles.py
 
-from turtle import *
+import turtle
 
 # maxRadius = int(input('Enter the max radius: '))
 # minRadius = int(input('Enter the min radius: '))
 # numCircles = int(input('Enter number of circles: '))
 
 t = turtle.Turtle()
+screen = turtle.Screen()
 
 # t.bgcolor("pink")
 t.penup()
-t.goto(0, -150)
+t.goto(-100, -150)
 t.pendown()
 
 t.pensize(2)
-t.pencolor(169, 169, 255)
-t.speed(6)
+# t.pencolor(169, 169, 255)
+t.pencolor("purple")
+t.speed(15)
 
 minRadius = 2
 maxRadius = 120
-input = 20
+num_circles = 20
 
-for i in range(input):
+'''
+Let's assume y is radius and x is i(index of circles):
+first, use linear equation to find the slope(the radius increment of each circle)
+m = (maxRadius - minRadius) / (last_i - first_i)
+second, find c(height) to locate where the line graph on matrix
+y = mx + c --> radius = m * i + c 
+'''
+
+#find slope, first and last circles are known:
+m = (maxRadius - minRadius) / 20 - 1
+# find height of the line graph:
+c = minRadius - m * 1 #use first circle; we can also use last circle to find c 
+
+# == First cone of circles:
+for i in range(num_circles):
   if i == 0:
     t.circle(minRadius)
-  elif (i == input -1):
+  elif (i == num_circles -1):
     t.circle(maxRadius)
   else:
-    t.circle(minRadius*i + (maxRadius-minRadius)/(input-2)) #this dividing does NOT create each circle in correct proportion 
-  
+    t.circle(m * i + c)
+    # t.circle(minRadius*i + (maxRadius-minRadius)/(num_circles-2)) #this dividing does NOT create each circle in correct proportion 
+
   t.penup()
   t.left(90)
-  t.forward(10)
+  t.forward(5)
   t.right(90)
   t.pendown()
+
+# == Second cone of cicirles:
+t.penup()
+t.goto(150, -150)
+t.pendown()
+t.pencolor("green")
+
+for i in range(1, num_circles + 1):
+  t.circle(m * i + c)
+
+  t.penup()
+  t.left(90)
+  t.forward(5)
+  t.right(90)
+  t.pendown()
+  print("i is ", i)
 	
+
+screen.exitonclick() #keep window open until it's terminated
+t.mainloop() #this will keep graphic window opened until window it's terminated
