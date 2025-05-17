6f7dce867d2dc61ef9f891ec2b26b5205d7e4e80
Anna <anna_liang_2@sfu.ca>
2025-05-16 23:20:14 -0700

    create random bubbles

--- randomBubbles.py
+++ randomBubbles.py
@@ -10,7 +10,7 @@ screen = turtle.Screen()
 
 screen.screensize(800,600)
 t.pensize(1)
-t.pencolor("aquamarine")
+# t.pencolor("aquamarine")
 t.speed(20)
 
 # for i in range(10):
@@ -28,31 +28,53 @@ colorlist = ["black", "grey", "darkgrey", "lightcoral", "brown", "maroon", "red"
 
 print("colors: ", len(colorlist))
 
-t.penup()
-t.goto(-300, 150)
-t.pendown()
-for i in range(len(colorlist)):
-    t.fillcolor(colorlist[i])
-    t.begin_fill()
-    t.circle(10)
-    t.end_fill()
+# t.penup()
+# t.goto(-300, 150)
+# t.pendown()
+# for i in range(len(colorlist)):
+#     t.fillcolor(colorlist[i])
+#     t.begin_fill()
+#     t.circle(10)
+#     t.end_fill()
+#     t.penup()
+#     t.forward(25)
+#     t.pendown()
+
+previous_color = 0
+rand_x_axis = 0
+
+
+while(rand_x_axis >= -350):
+# for i in range(5):
+    # random location:
+    rand_x_axis = random.randint(-370, 600)
+    rand_y_axis = random.randint(-250, 250)
+    # print(rand_x_axis, " ", rand_y_axis)
+    
     t.penup()
-    t.forward(25)
+    t.goto(rand_x_axis, rand_y_axis)
     t.pendown()
 
-# while(rand_x_axis != -350):
-#     # random location:
-#     rand_x_axis = random.randint(-800, 800)
-#     rand_y_axis = random.randint(-600, 600)
-#     print(rand_x_axis, " ", rand_y_axis)
+    #random color:
+    random_color = random.randint(0, len(colorlist)-1)
+    while(random_color == previous_color):
+        random_color = random.randint(0, len(colorlist)-1)
+    t.fillcolor(colorlist[random_color])
+    t.pencolor(colorlist[random_color])
 
-#     #random size:
-#     random.randint(10,100)
+    #random size:
+    rand_radius = random.randint(10,100)
+    t.begin_fill()
+    t.circle(rand_radius)
+    t.end_fill()
 
+    previous_color = random_color
+    # print("previous color index: ", previous_color)
 
-
-
-
+t.penup()
+t.home()
+t.shape("turtle")
+t.shapesize(2, 2)
 
 screen.exitonclick() #keep window open until it's terminated
 # t.mainloop() #this will keep graphic window opened until window it's terminated
