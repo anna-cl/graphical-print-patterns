ae1b4a57eb41636c83a5d953141ec458bc24bbfb
Anna <anna_liang_2@sfu.ca>
2025-05-16 22:41:46 -0700

    testing colors

--- randomBubbles.py
+++ randomBubbles.py
@@ -11,20 +11,44 @@ screen = turtle.Screen()
 screen.screensize(800,600)
 t.pensize(1)
 t.pencolor("aquamarine")
-t.speed(10)
+t.speed(20)
 
-print(random, type(random))
-
-for i in range(10):
-    rand_x_axis = random.randint(-500, 800)
+# for i in range(10):
+#     rand_x_axis = random.randint(-800, 800)
     
-    rand_y_axis = random.randint(-500, 600)
+#     rand_y_axis = random.randint(-600, 600)
     
+#     print(rand_x_axis, " ", rand_y_axis)
 
-    print(rand_x_axis, " ", rand_y_axis)
+# 41 colors:
+colorlist = ["black", "grey", "darkgrey", "lightcoral", "brown", "maroon", "red", "tomato", "orangered", "saddlebrown", "peru",
+             "darkorange", "tan", "wheat", "goldenrod", "gold", "khaki", "olive", "yellow", "greenyellow", "limegreen", "green",
+             "aquamarine", "lightseagreen", "paleturquoise", "teal", "cyan", "skyblue", "dodgerblue", "royalblue", "lavender", 
+             "navy", "blue", "blueviolet", "darkviolet", "purple", "fuchsia", "deeppink", "hotpink", "crimson", "pink"]
+
+print("colors: ", len(colorlist))
+
+t.penup()
+t.goto(-300, 150)
+t.pendown()
+for i in range(len(colorlist)):
+    t.fillcolor(colorlist[i])
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+    t.penup()
+    t.forward(25)
+    t.pendown()
 
 # while(rand_x_axis != -350):
-    
+#     # random location:
+#     rand_x_axis = random.randint(-800, 800)
+#     rand_y_axis = random.randint(-600, 600)
+#     print(rand_x_axis, " ", rand_y_axis)
+
+#     #random size:
+#     random.randint(10,100)
+
 
 
 
